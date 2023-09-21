86e752c1ec2e12bc4e7f9f348bf038bb8286dc1f
MISHA <vol.mykhailo@gmail.com>
2023-09-21 23:02:17 +0300

    start

---
+++ DinArr.h
@@ -0,0 +1,26 @@
+#pragma once
+class DinArr
+{
+private:
+	int* pointer;
+	int siz;
+public:
+	DinArr();
+	DinArr(int);
+	DinArr(const DinArr);
+	~DinArr();
+
+	void Input();
+	void PrintConsole();
+
+	DinArr operator+(int x);
+	DinArr operator-(int x);
+	DinArr operator*(int x);
+	DinArr operator-(DinArr result2);
+	DinArr operator+(DinArr y);
+	DinArr operator++();
+	DinArr operator--();
+
+	int* GetPointer();
+	int GetSize();
+};
